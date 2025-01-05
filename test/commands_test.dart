d0a2d08d84dcd6f906fc750b3b12ec149ed4aa25
Rasmus Pedersen <18396856+rpede@users.noreply.github.com>
2025-01-05 20:52:48 +0100

    Solution

--- test/commands_test.dart
+++ test/commands_test.dart
@@ -3,26 +3,86 @@ import 'package:calculator_cli/internal_state.dart';
 import 'package:test/test.dart';
 
 void main() {
-  /* TODO write tests for commands
-   *
-   * - Enter
-   *   - a new stack with given value at the end and old stack in history
-   * - Clear
-   *   - an empty state
-   * - Undo
-   *   - previous state restored from history
-   * - Add
-   *   - stack with the last two values added, and history so that the old state can be restored
-   *   - does nothing when stack length is less than 2
-   * - Subtract
-   *   - stack with the last two values subtracted, and history so that old state
-   *     can be restored
-   *   - does nothing when stack length is less than 2
-   * - Multiply
-   *   - stack with the last two values multiplied, and history so that old state
-   *   - does nothing when stack length is less than 2
-   * - Divide
-   *   - stack with the last two values divided, and history so that old state
-   *   - does nothing when stack length is less than 2
-  */
+  group("Enter", () {
+    test("a new stack with given value at the end and old stack in history",
+        () {
+      final origState = InternalState.empty();
+      final newState = Enter(1).apply(origState);
+
+      expect(newState, isNot(origState));
+      expect(newState.stack, equals([1]));
+      expect(newState.history, hasLength(1));
+    });
+  });
+
+  group("Clear", () {
+    test("an empty state", () {
+      final origState = InternalState(stack: [1], history: []);
+      final newState = Clear().apply(origState);
+
+      expect(newState, isNot(origState));
+      expect(newState.stack, isEmpty);
+      expect(newState.history, isEmpty);
+    });
+  });
+
+  group("Undo", () {
+    test("previous state restored from history", () {
+      final origState = InternalState(stack: [
+        1,
+        2
+      ], history: [
+        [1]
+      ]);
+      final newState = Undo().apply(origState);
+
+      expect(newState, isNot(origState));
+      expect(newState.stack, equals([1]));
+      expect(newState.history, isEmpty);
+    });
+  });
+
+  group("Add", () {
+    test('adds the last two values', () {
+      final origState = InternalState(stack: [1, 2], history: []);
+      final newState = Add().apply(origState);
+
+      expect(newState, isNot(origState));
+      expect(newState.stack, equals([3]));
+      expect(newState.history, hasLength(1));
+    });
+
+    test("does nothing when stack length is less than 2", () {
+      final origState = InternalState(stack: [1], history: []);
+      final newState = Add().apply(origState);
+      expect(newState, equals(origState));
+    });
+  });
+
+  group("Subtract", () {
+    test('Subtract the last two values', () {
+      final origState = InternalState(stack: [1, 2], history: []);
+      final newState = Subtract().apply(origState);
+
+      expect(newState.stack, equals([-1]));
+    });
+  });
+
+  group("Multiply", () {
+    test('Multiply the last two values', () {
+      final origState = InternalState(stack: [2, 3], history: []);
+      final newState = Multiply().apply(origState);
+
+      expect(newState.stack, equals([6]));
+    });
+  });
+
+  group("Divide", () {
+    test('Divide the last two values', () {
+      final origState = InternalState(stack: [21, 7], history: []);
+      final newState = Divide().apply(origState);
+
+      expect(newState.stack, equals([3]));
+    });
+  });
 }
